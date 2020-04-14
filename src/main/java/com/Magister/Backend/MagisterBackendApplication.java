61d44f1c975948d51ba2e1aa9fa1bc4b40bc6346
unknown <hiagosouzadeabreu@gmail.com>
2020-04-14 19:58:26 -0300

    Projeto Java Spring padrão

---
+++ src/main/java/com/Magister/Backend/MagisterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Magister.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagisterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagisterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Magister/Backend/MagisterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Magister.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagisterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
